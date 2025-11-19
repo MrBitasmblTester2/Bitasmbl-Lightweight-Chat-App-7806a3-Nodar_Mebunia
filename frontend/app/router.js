5385f3fe7801ef557bfad65b782b4ae6843adb2f
MrBitasmblTester2 <lulu2214@proton.me>
2025-11-19 14:28:53 +0400

    Add router.js

---
+++ frontend/app/router.js
@@ -0,0 +1,5 @@
+import EmberRouter from '@ember/routing/router';
+import config from '../config/environment';
+const Router = EmberRouter.extend({ location: config.locationType });
+Router.map(function(){ this.route('room',{ path: '/:room' }); });
+export default Router;
